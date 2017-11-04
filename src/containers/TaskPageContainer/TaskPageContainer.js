7f02970f99431512c4d6ad20701da93c8e5a56b1
SergeyShatter <siarhei.ausianik@gmail.com>
2017-11-04 18:54:09 +0300

    (CodeEditor):Primary integration with the Ace code editor.

--- src/containers/TaskPageContainer/TaskPageContainer.js
+++ src/containers/TaskPageContainer/TaskPageContainer.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import {Grid, Row} from 'react-bootstrap';
-import {TaskDescription} from '../../components/index';
+import {TaskDescription, CodeEditor} from '../../components/index';
 import style from './taskPageContainer.scss';
 
 export default class TaskPage extends Component {
@@ -9,7 +9,7 @@ export default class TaskPage extends Component {
             <Grid className={style.taskPageContainer} fluid={true}>
                 <Row className="showGrid">
                     <TaskDescription />
-                    {/* <CodeEditor />*/}
+                    <CodeEditor />
                 </Row>
             </Grid>
         );
