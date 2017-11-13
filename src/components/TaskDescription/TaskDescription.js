aaadf71f21995686785cf501df92cfa52ad11178
Sergey Ovsyanik <siarhei.shatter@gmail.com>
2017-11-13 23:40:06 +0300

    Added ability to change language, theme, fontSize.

--- src/components/TaskDescription/TaskDescription.js
+++ src/components/TaskDescription/TaskDescription.js
@@ -1,10 +1,9 @@
 import React, {Component} from 'react';
-import style from './taskDescription.scss';
 
 export default class TaskDescription extends Component {
     render() {
         return (
-            <div className={style.descriptionContainer}>
+            <div>
                 {this.props.description}
             </div>
         );
--- src/components/TaskOutput/TaskOutput.js
+++ src/components/TaskOutput/TaskOutput.js
@@ -1,10 +1,9 @@
 import React, {Component} from 'react';
-import style from './taskOutput.scss';
 
 export default class TaskOutput extends Component {
     render() {
         return (
-            <div className={style.outputContainer}>
+            <div>
                 {this.props.output}
             </div>
         );
