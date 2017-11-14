417bf0c1d46f08da88243e246c21b1651dda65ff
Sergey Ovsyanik <siarhei.shatter@gmail.com>
2017-11-14 23:29:21 +0300

    Added sample tests editor

--- src/components/CodeEditor/CodeEditor.js
+++ src/components/CodeEditor/CodeEditor.js
@@ -18,11 +18,11 @@ export default class CodeEditor extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            value: '',
-            mode: 'JavaScript',
-            theme: 'Twilight',
-            fontSize: 16,
-            highlightActiveLine: true
+            value: this.props.value,
+            mode: this.props.mode,
+            theme: this.props.theme,
+            fontSize: this.props.fontSize,
+            highlightActiveLine: this.props.highlightActiveLine
         };
     }
 
@@ -47,19 +47,25 @@ export default class CodeEditor extends Component {
     render() {
         return (
             <section>
-                <ButtonGroup justified>
+                <ButtonGroup block>
                     <DropdownButton
-                        title={`Language: ${this.state.mode}`}
+                        id={'dropdown-language'}
                         key={'dropdown-language'}
+                        title={`Language: ${this.state.mode}`}
+                        bsSize={'large'}
                         onSelect={this.setMode}
                     >
-                        <MenuItem eventKey={'JavaScript'}>JavaScript</MenuItem>
+                        <MenuItem eventKey={'JavaScript'}>
+                            JavaScript
+                        </MenuItem>
                         <MenuItem eventKey={'CSS'}>CSS</MenuItem>
                         <MenuItem eventKey={'HTML'}>HTML</MenuItem>
                     </DropdownButton>
                     <DropdownButton
-                        title={`Theme: ${this.state.theme}`}
+                        id={'dropdown-theme'}
                         key={'dropdown-theme'}
+                        title={`Theme: ${this.state.theme}`}
+                        bsSize={'large'}
                         onSelect={this.setTheme}
                     >
                         <MenuItem eventKey={'Twilight'}>Twilight</MenuItem>
@@ -67,8 +73,10 @@ export default class CodeEditor extends Component {
                         <MenuItem eventKey={'Monokai'}>Monokai</MenuItem>
                     </DropdownButton>
                     <DropdownButton
-                        title={`Font size: ${this.state.fontSize}`}
+                        id={'dropdown-fontSize'}
                         key={'dropdown-fontSize'}
+                        title={`Font size: ${this.state.fontSize}`}
+                        bsSize={'large'}
                         onSelect={this.setFontSize}
                     >
                         <MenuItem eventKey={14}>14</MenuItem>
@@ -77,6 +85,7 @@ export default class CodeEditor extends Component {
                         <MenuItem eventKey={20}>20</MenuItem>
                     </DropdownButton>
                 </ButtonGroup>
+                <h3>Solution:</h3>
                 <AceEditor
                     value={this.state.value}
                     mode={this.state.mode.toLowerCase()}
@@ -84,7 +93,20 @@ export default class CodeEditor extends Component {
                     fontSize={this.state.fontSize}
                     highlightActiveLine={this.state.highlightActiveLine}
                     width={'100%'}
-                    height={'50vh'}
+                    height={'44vh'}
+                    showPrintMargin={false}
+                    onChange={this.onChange}
+                />
+                <h3>Sample Tests:</h3>
+                <AceEditor
+                    value={this.state.value}
+                    mode={this.state.mode.toLowerCase()}
+                    theme={this.state.theme.toLowerCase()}
+                    fontSize={this.state.fontSize}
+                    highlightActiveLine={this.state.highlightActiveLine}
+                    width={'100%'}
+                    height={'20vh'}
+                    showPrintMargin={false}
                     onChange={this.onChange}
                 />
             </section>
--- src/containers/TaskContainer.js
+++ src/containers/TaskContainer.js
@@ -11,39 +11,57 @@ import TaskOutput from '../components/TaskOutput/TaskOutput';
 import CodeEditor from '../components/CodeEditor/CodeEditor';
 
 export default class TaskContainer extends React.Component {
-  constructor() {
-    super();
-
+  constructor(props) {
+    super(props);
     this.state = {
-        description: 'Task Description',
-        output: 'Task Output'
+        task: {
+            description: 'Task Description',
+            output: 'Task Output'
+        },
+        codeEditor: {
+            value: 'function reverseWords(str){\n' +
+            '  return str; // reverse those words\n' +
+            '}',
+            mode: 'JavaScript',
+            theme: 'Twilight',
+            fontSize: 16,
+            highlightActiveLine: true
+        }
     };
   }
 
   render() {
       return (
-        <Grid fluid={true}>
-            <h1>Task : {this.props.match.params.taskId}</h1>
-            <Row>
-                <Col md={5}>
-                    <Tabs id={'taskDescriptionTabs'} defaultActiveKey={1}>
-                        <Tab eventKey={1} title={'Description'}>
-                            <TaskDescription
-                                description={this.state.description}
-                            />
-                        </Tab>
-                        <Tab eventKey={2} title={'Output'}>
-                            <TaskOutput
-                                output={this.state.output}
-                            />
-                        </Tab>
-                    </Tabs>
-                </Col>
-                <Col md={7}>
-                    <CodeEditor />
-                </Col>
-            </Row>
-        </Grid>
+            <Grid fluid={true}>
+                <Row>
+                    <Col md={5}>
+                        <h1>Task : {this.props.match.params.taskId}</h1>
+                        <Tabs id={'taskDescriptionTabs'} defaultActiveKey={1}>
+                            <Tab eventKey={1} title={'Description'}>
+                                <TaskDescription
+                                    description={this.state.description}
+                                />
+                            </Tab>
+                            <Tab eventKey={2} title={'Output'}>
+                                <TaskOutput
+                                    output={this.state.output}
+                                />
+                            </Tab>
+                        </Tabs>
+                    </Col>
+                    <Col md={7}>
+                        <CodeEditor
+                            value={this.state.codeEditor.value}
+                            mode={this.state.codeEditor.mode}
+                            theme={this.state.codeEditor.theme}
+                            fontSize={this.state.codeEditor.fontSize}
+                            highlightActiveLine={
+                                this.state.codeEditor.highlightActiveLine
+                            }
+                        />
+                    </Col>
+                </Row>
+            </Grid>
       );
   }
 }
