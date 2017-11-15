71d6ddc849eb80625d297801f4f9679a51107581
Rufat Khaslarov <rufat.khaslarov@gmail.com>
2017-11-15 12:30:25 +0300

    Create action for tours error

--- src/sagas/tournamentSaga.js
+++ src/sagas/tournamentSaga.js
@@ -1,7 +1,7 @@
 import {call, put, takeEvery} from 'redux-saga/effects';
 import Api from '../api/tournaments';
 
-import {fetchSuccess} from '../actions/tournamentActions';
+import {fetchSuccess, fetchFailed} from '../actions/tournamentActions';
 import {TOURNAMENTS_FETCH_REQUESTED} from '../constants';
 
 function* fetchTournaments() {
@@ -9,7 +9,7 @@ function* fetchTournaments() {
       const tournaments = yield call(Api.fetchAll);
       yield put(fetchSuccess(tournaments));
    } catch (e) {
-      yield put({type: 'TOURS_FETCH_FAILED', message: e.message}); // toDO
+      yield put(fetchFailed(e));
    }
 }
 
