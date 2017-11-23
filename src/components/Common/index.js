4bf08262857b8239275df1ee5c75e84f92e80364
Rufat Khaslarov <rufat.khaslarov@gmail.com>
2017-11-23 12:21:08 +0300

    [login] move from common

--- src/components/Common/index.js
+++ src/components/Common/index.js
@@ -1,7 +1,6 @@
-import Login from './Login/Login';
 import ProgressBar from './ProgressBar/ProgressBar';
 
 export {
-  Login,
+
   ProgressBar
 };
--- src/containers/LoginContainer.js
+++ src/containers/LoginContainer.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Login} from '../components/Common';
+import Login from '../components/Login/Login';
 import {
   Grid,
   Row,
