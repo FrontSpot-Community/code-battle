ccf291d16d38f86a8c352733300c0bf759ecc361
Rufat Khaslarov <rufat.khaslarov@gmail.com>
2017-11-23 10:45:58 +0300

    [login] route and container

---
+++ src/containers/LoginContainer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class LoginContainer extends React.Component {
+    render() {
+        return (
+            <div>
+              Login form
+            </div>
+        );
+    }
+}
--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import HomeContainer from './containers/HomeContainer';
 import AboutContainer from './containers/AboutContainer';
 import BattleContainer from './containers/BattleContainer';
 import TaskContainer from './containers/TaskContainer';
-import TaskTrainContainer from './containers/TaskTrainContainer';
+import LoginContainer from './containers/LoginContainer';
 import rootSaga from './sagas';
 
 const store = configureStore();
@@ -25,6 +25,7 @@ render(
                 <Switch>
                     <Route exact path="/" component={HomeContainer} />
                     <Route exact path="/about" component={AboutContainer} />
+                    <Route exact path="/login" component={LoginContainer} />
                     <Route exact
                            path="/battle/:id"
                            component={BattleContainer} />
