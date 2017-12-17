0c48b05c514a26c7bb5396880ed8483bc1d7cad4
Sergey Ovsyanik <siarhei.shatter@gmail.com>
2017-12-18 01:18:44 +0300

    Png logo replaced by svg version

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,18 +3,20 @@ import {Link} from 'react-router-dom';
 
 import Breadcrumbs from './components/Breadcrumbs';
 
-import codeBattleLogo from '../../assets/images/code-battle-logo.png';
+import logo from '../../assets/images/logo.svg';
 import styles from './header.scss';
 
 class Header extends Component {
   render() {
     return (
-      <header>
+      <header className={styles.header}>
         <Link to="/" className={styles.logoWrapper}>
-          <img src={codeBattleLogo} className={styles.logo}/>
+          <img src={logo} className={styles.logo}/>
           CODE BATTLE
         </Link>
         <Breadcrumbs />
+        {/* <Notifications />*/}
+        {/* <ProfileItem />*/}
       </header>
     );
   }
