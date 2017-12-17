bf2caa5eb235dcd46af93a8407ad4e56e4f4d168
vladbitgroup <vlad@bitgroup.co.il>
2017-12-17 04:29:48 +0300

    refactoring_1

--- src/containers/HomeContainer/HomeContainer.js
+++ src/containers/HomeContainer/HomeContainer.js
@@ -9,32 +9,30 @@ import Rank from '../../components/Rank';
 import userScoreList from './mockUsers';
 
 class HomeContainer extends React.Component {
-  constructor(props) {
-    super(props);
-  }
+    constructor(props) {
+        super(props);
+    }
 
-  render() {
-      return (
-        <div className={style.wrapper}>
-            <div className={style.tableContainer}>
+    render() {
+        return (
+            <div className={style.wrapper}>
+
                 <TournamentTable tournaments={this.props.tournaments}
                                  render={() => (<Tabs/>)}/>
-            </div>
-            <div className={style.rankContainer}>
+
                 <Rank rankPosition={102}
                       totalRankPosition={654}
                       totalScore={190354}
                       userScoreList={userScoreList}/>
             </div>
-        </div>
-      );
-  }
+        );
+    }
 }
 
 const mapStateToProps = (state) => {
-  return {
-    tournaments: state.tournaments.data
-  };
+    return {
+        tournaments: state.tournaments.data
+    };
 };
 
 export default connect(mapStateToProps)(HomeContainer);
