1021409b407562d3c2d18342f7174c97bcb8487b
Sergey Ovsyanik <siarhei.ausianik@gmail.com>
2017-12-27 21:08:45 +0300

    Implemented run tests block.

--- src/components/CodeEditor/CodeEditor.js
+++ src/components/CodeEditor/CodeEditor.js
@@ -1,9 +1,4 @@
 import React, {Component} from 'react';
-import {
-  ButtonGroup,
-  DropdownButton,
-  MenuItem
-} from 'react-bootstrap';
 import AceEditor from 'react-ace';
 
 import 'brace/mode/javascript';
@@ -18,80 +13,25 @@ export default class CodeEditor extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      value: '',
       mode: 'JavaScript',
-      theme: 'Twilight',
-      fontSize: 16,
+      theme: 'monokai',
+      fontSize: 14,
       highlightActiveLine: true
     };
   }
 
-  onChange = (newValue) => {
-    this.setState({
-      value: newValue
-    });
-
-    if (typeof this.props.onChange === 'function') {
-      this.props.onChange(newValue);
-    }
-  };
-
-  setMode = (mode) => {
-    this.setState({mode});
-  };
-
-  setTheme = (theme) => {
-    this.setState({theme});
-  };
-
-  setFontSize = (fontSize) => {
-    this.setState({fontSize});
-  };
-
   render() {
     return (
-      <section>
-        <ButtonGroup justified>
-          <DropdownButton
-            title={`Language: ${this.state.mode}`}
-            key={'dropdown-language'}
-            onSelect={this.setMode}
-          >
-            <MenuItem eventKey={'JavaScript'}>JavaScript</MenuItem>
-            <MenuItem eventKey={'CSS'}>CSS</MenuItem>
-            <MenuItem eventKey={'HTML'}>HTML</MenuItem>
-          </DropdownButton>
-          <DropdownButton
-            title={`Theme: ${this.state.theme}`}
-            key={'dropdown-theme'}
-            onSelect={this.setTheme}
-          >
-            <MenuItem eventKey={'Twilight'}>Twilight</MenuItem>
-            <MenuItem eventKey={'Tomorrow'}>Tomorrow</MenuItem>
-            <MenuItem eventKey={'Monokai'}>Monokai</MenuItem>
-          </DropdownButton>
-          <DropdownButton
-            title={`Font size: ${this.state.fontSize}`}
-            key={'dropdown-fontSize'}
-            onSelect={this.setFontSize}
-          >
-            <MenuItem eventKey={14}>14</MenuItem>
-            <MenuItem eventKey={16}>16</MenuItem>
-            <MenuItem eventKey={18}>18</MenuItem>
-            <MenuItem eventKey={20}>20</MenuItem>
-          </DropdownButton>
-        </ButtonGroup>
-        <AceEditor
-          value={this.state.value}
-          mode={this.state.mode.toLowerCase()}
-          theme={this.state.theme.toLowerCase()}
-          fontSize={this.state.fontSize}
-          highlightActiveLine={this.state.highlightActiveLine}
-          width={'100%'}
-          height={'50vh'}
-          onChange={this.onChange}
-        />
-      </section>
+      <AceEditor
+        value={this.props.value}
+        mode={this.state.mode.toLowerCase()}
+        theme={this.state.theme.toLowerCase()}
+        fontSize={this.state.fontSize}
+        highlightActiveLine={this.state.highlightActiveLine}
+        width={'100%'}
+        height={'100%'}
+        onChange={this.props.onCodeEditorChange}
+      />
     );
   }
 }
--- src/containers/TaskTrainContainer/TaskTrainContainer.js
+++ src/containers/TaskTrainContainer/TaskTrainContainer.js
@@ -1,28 +1,29 @@
 import React from 'react';
 import {connect} from 'react-redux';
-import {
-  Grid,
-  Row,
-  Col,
-  Tabs,
-  Tab
-} from 'react-bootstrap';
 
-import TaskDescription from '../../components/TaskDescription/TaskDescription';
-import TaskOutput from '../../components/TaskOutput/TaskOutput';
+// import TaskDescription from '../../components/TaskDescription/TaskDescription';
+// import TaskOutput from '../../components/TaskOutput/TaskOutput';
 import CodeEditor from '../../components/CodeEditor/CodeEditor';
 import Button from '../../components/Common/Button';
 import {solutionRequest} from '../../actions/action_creators/solutionActionCreators';
 import {bindActionCreators} from 'redux';
 
+import style from './style.scss';
+
 class TaskTrainContainer extends React.Component {
   constructor() {
     super();
-
     this.state = {
       description: 'Task Description',
-      output: 'Task Output',
-      solution: ''
+      solution: 'function findEvenIndex(arr)\n' +
+      '{\n' +
+      '    //Code goes here!\n' +
+      '}',
+      sampleTests: 'Test.describe("FindEvenIndex", function() {\n' +
+      '  Test.it("Tests", function() {\n' +
+      '    Test.assertEquals(findEvenIndex([1,2,3,4,5,6]),-1, "The array: [1,2,3,4,5,6] \\n");\n' +
+      '  });\n' +
+      '});'
     };
   }
 
@@ -33,43 +34,56 @@ class TaskTrainContainer extends React.Component {
     });
   };
 
+  runTests = () => {
+    // run test action
+  };
+
   onCodeEditorChange = (newText) => {
     this.setState({
       solution: newText
     });
   };
 
+  resetSolution = () => this.setState({solution: ''});
+
   render() {
-    const {solution} = this.props;
-    const solutionOutput = solution && solution.runOutput || '';
+    // const {solution} = this.props;
+
+    // const solutionOutput = solution && solution.runOutput || '';
+
     return (
-      <Grid fluid={true}>
-        <h1>Task : {this.props.match.params.taskId}</h1>
-        <Row>
-          <Col md={5}>
-            <Tabs id={'taskDescriptionTabs'} defaultActiveKey={1}>
-              <Tab eventKey={1} title={'Description'}>
-                <TaskDescription
-                  description={this.state.description}
-                />
-              </Tab>
-              <Tab eventKey={2} title={'Output'}>
-                <TaskOutput
-                  output={solutionOutput}
-                />
-              </Tab>
-            </Tabs>
-          </Col>
-          <Col md={7}>
+      <div className={style.container}>
+        <div className={style.taskName}>{this.props.match.params.id}</div>
+        <div className={style.row}>
+          <div className={style.contentBlock}>
+            <div className={style.contentBlockHeader}>
+              <span className={style.contentBlockName}>SOLUTION</span>
+              <div className={style.contentBlockHeaderRightArea}>
+                <button onClick={this.resetSolution} className={style.resetButton}>
+                  <span className={style.resetButtonText}>RESET</span>
+                </button>
+                <Button onClick={this.submitTask}>SUBMIT</Button>
+              </div>
+            </div>
             <CodeEditor
-              onChange={this.onCodeEditorChange}
+              value={this.state.solution}
+              onCodeEditorChange={this.onCodeEditorChange}
             />
-          </Col>
-        </Row>
-        <Row>
-          <Button onClick={this.submitTask}>SUBMIT</Button>
-        </Row>
-      </Grid>
+          </div>
+          <div className={style.contentBlock}>
+            <div className={style.contentBlockHeader}>
+              <span className={style.contentBlockName}>SAMPLE TESTS</span>
+              <button onClick={this.runTests} className={style.runTestsButton}>
+                <span className={style.runTestsButtonText}>RUN TESTS</span>
+              </button>
+            </div>
+            <CodeEditor
+              value={this.state.sampleTests}
+              onCodeEditorChange={this.onCodeEditorChange}
+            />
+          </div>
+        </div>
+      </div>
     );
   }
 }
